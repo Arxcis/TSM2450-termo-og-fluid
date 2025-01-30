36ed952032d422cb7d54020339dc08120f825566
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-01-30 19:06:50 +0100

    Make code more pretty

--- oppg2.py
+++ oppg2.py
@@ -1,95 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+TSM2450 Fluidmekanikk Oblig 1
+Oppgave 2
+
+Studentnr: 267431@usn.no 
+"""
+from dataclasses import dataclass
+
+@dataclass
+class Rør:
+    """Representerer et rør som skal levere luft til et rom"""
+    # Verdier som gis
+    navn: str       
+    areal: float    # m2 - areal til rommet som røret skal levere til
+    lengde: float   # m  - lengde til røret
+
+    # Verdier som skal beregnes
+    personer: float = None #      - antall personer gitt arealet til rommet
+    Q: float        = None # m3/s - volumflyt gitt antall personer
+    A_min: float    = None # m2   - minste tverrsnitt gitt Q
+    A_hylle: float  = None # m2   - hylletverrsnitt gitt minste tverrsnitt
+    v_hylle: float  = None # m/s  - fart gitt hylletverrsnitt  
+    volum: float    = None # m3   - volum gitt lengde og hylletverrsnitt
+
 
 def oppg2():
-    """
-    TSM2450 Fluidmekanikk Oblig 1
-    Oppgave 2
-    
-    Studentnr: 267431@usn.no 
-    """
+    """Stiller inn rør, beregner alle verdier og skriver til skjerm"""
     #
-    # 1. Configure
+    # 1. Innstillinger
     #
-    personer_per_areal   = 0.7          # person/m2
-    Q_luft_pers_per_time = 26           # m3/t
-    v_max                = 10           # m/s
-    rør = [
-        ("hoved",       1.5, 160+80+80+80+80+70+40+40),
+    Personer_per_areal   = 0.7 # person/m2
+    Q_luft_pers_per_time = 26  # m3/t
+    V_max                = 10  # m/s
 
-        ("vestre",     15.0, 160+80+80),
-        ("vestre 160",  5.0, 160),
-        ("vestre 80",   5.0, 80),
-        ("vestre 80",   5.0, 80),
+    Røra = [
+        Rør(navn="hoved",      lengde=1.5,  areal=160+80+80+80+80+70+40+40),
+
+        Rør(navn="vestre",     lengde=15.0, areal=160+80+80),
+        Rør(navn="vestre 160", lengde=5.0,  areal=160),
+        Rør(navn="vestre 80",  lengde=5.0,  areal=80),
+        Rør(navn="vestre 80",  lengde=5.0,  areal=80),
         
-        ("østre",    15.0, 80+80+70+40+40),
-        ("østre 80", 5.0, 480),
-        ("østre 80", 5.0, 80),
-        ("østre 70", 4.5, 70),
-        ("østre 40", 5.0, 40),
+        Rør(navn="østre",    lengde=15.0, areal=80+80+70+40+40),
+        Rør(navn="østre 80", lengde=5.0,  areal=480),
+        Rør(navn="østre 80", lengde=5.0,  areal=80),
+        Rør(navn="østre 70", lengde=4.5,  areal=70),
+        Rør(navn="østre 40", lengde=5.0,  areal=40),
     ]
+    Areal = røra[0].areal
 
     #
-    # 2. Compute
+    # 2. Beregninger
     #
     Q_luft_pers_per_sekund = Q_luft_pers_per_time / 3600  # m3/pers/s
     
-    def beregn_rør(navn: str, lengde: float, areal:int):
-        personer = areal * personer_per_areal
-        Q = Q_luft_pers_per_sekund * personer
+    for rør in Røra:
+        rør.personer = rør.areal * Personer_per_areal
+        rør.Q = Q_luft_pers_per_sekund * rør.personer
+        rør.A_min = rør.Q / V_max
+        rør.A_hylle, err = velg_hylletverrsnitt(rør.A_min)
 
-        A_min = Q / v_max
-        A_hylle, err = velg_hyllerør(A_min)
         if err:
             print(f"Feil med {navn}: ", err)
             exit(1)
+        
+        rør.v_hylle = rør.Q / A_hylle,
+        rør.volum = rør.A_hylle * rør.lengde
 
-        return {
-            "navn": navn,
-            "Q": Q,
-            "areal": areal,
-            "A_min": A_min,
-            "A_hylle": A_hylle,
-            "v_max": Q / A_min,
-            "v_hylle": Q / A_hylle,
-            "lengde": lengde,
-            "volum": A_hylle * lengde,
-        } 
-
-    rør        = [beregn_rør(*r) for r in rør]
-    areal  = rør[0]["areal"]
-    rør_lengde = sum([r["lengde"] for r in rør])              # m
-    rør_volum  = sum([r["lengde"]*r["A_hylle"] for r in rør]) # m3
+    rørlengde_total = sum([rør.lengde for rør in Røra])             # m
+    rørvolum_total  = sum([rør.lengde*rør.A_hylle for rør in Røra]) # m3
 
     #
     # 3. Pretty print
     #
     from pandas import DataFrame
-    from math import ceil
 
     print(f"""
-Oppg2 svar:
+#
+# Oppg2 svar:
+#
 
     Innstillinger:
-            v_max            =  {v_max} m/s
-            areal            = {areal} m2
+        V max            =  {V_max} m/s
+        Q luft per time  = {Q_luft_pers_per_time:>6.1f} m3/person/t
+        Q luft per pers  = {Q_luft_pers_per_sekund:>6.4f} m3/person/s
 
-            Q luft per time  = {Q_luft_pers_per_time:>6.1f} m3/person/t
-            Q luft per pers  = {Q_luft_pers_per_sekund:>6.4f} m3/person/s
+        {print(DataFrame({
+            "Lengde": (rør.lengde for rør in Røra),
+            "Areal": (rør.areal for rør in Røra),
+        }, index=(rør.navn for rør in Røra)))}
 
-    Rør:
-        Total lengde = {rør_lengde:>5.2f} m
-        Total volum  = {rør_volum:>5.2f} m3
-          """)
+    Resultater:
+        Total lengde = {rørlengde_total:>5.2f} m
+        Total volum  = {rørvolum_total:>5.2f} m3
+        
+        {print(DataFrame({
+            "#Personer": (rør.personer for rør in Røra),
+            "Q [m3/s]": (round(rør.Q, 3) for rør in Røra),
+            "A min [m2]": (round(rør.A_min, 3) for rør in Røra),
+            "A hylle [m2]": (round(rør.A_hylle, 3) for rør in Røra),
+            "v hylle [m/s]": (round(rør.v_hylle, 3) for rør in Røra),
+            "volum [m3]": (round(rør.volum, 3) for rør in Røra),
+        }, index=(r.navn for rør in Røra)))}
+              
+""")
 
-    print(DataFrame({
-        "Rom [m2]": (["areal"] for r in rør),
-        "Q [m3/s]": (round(r["Q"], 3) for r in rør),
-        "Lengde [m]": (r["lengde"] for r in rør),
-        "A min [m2]": (round(r["A_min"], 3) for r in rør),
-        "A hylle [m2]": (round(r["A_hylle"], 3) for r in rør),
-        "v hylle [m/s]": (round(r["v_hylle"], 3) for r in rør),
-
-        }, index=(r["navn"] for r in rør)))
-       
 
 HYLLE = [
     0.003, # m2
@@ -109,7 +125,7 @@ HYLLE = [
     2.011,
 ]    
 
-def velg_hyllerør(A_min: float):
+def velg_hylletverrsnitt(A_min: float):
     """
     A_min: float - minsteverdi for ønske av rør. Velger tilsvarende rør eller nærmeste rør som er større enn denne verdien.
     """
@@ -123,5 +139,3 @@ def velg_hyllerør(A_min: float):
 
 if __name__ == "__main__":
     oppg2()
-
-
